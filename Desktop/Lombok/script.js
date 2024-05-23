0701b5ce1ca8a78872308f68bc3e5ec56b6f5380
AbetGregana <ansbert.gregana@frontlinebusiness.com.ph>
2024-05-23 13:41:22 +0800

    card2 replaced

--- Desktop/Lombok/script.js
+++ Desktop/Lombok/script.js
@@ -1,6 +1,16 @@
-const toggleMenu = document.querySelector(".toggle__menu");
+const burgir = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav");
-toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
+burgir.addEventListener("click", () => {
+  burgir.classList.toggle("open");
   headerNav.classList.toggle("open");
+});
+
+const headerW = document.querySelector(".header__wrapper");
+console.log.window;
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    headerW.classList.add("open");
+  } else {
+    headerW.classList.remove("open");
+  }
 });
